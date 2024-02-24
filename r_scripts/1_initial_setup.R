6ebe27bccf599c780ef47da52d271b3d702b6642
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-02-24 01:31:12 -0600

    Organization of scripts and directories

---
+++ r_scripts/1_initial_setup.R
@@ -0,0 +1,34 @@
+# L07 Baseline Models: Exercise 1 ----
+# Initial data checks & data splitting
+
+# Random process in script, seed set right before it
+
+# load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+library(patchwork)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# load data ----
+
+
+# inspecting target variable
+
+
+# quick data quality ----
+
+# initial split ----
+# set seed
+set.seed(3012)
+
+# folding data (resamples) ----
+# set seed 
+set.seed(605)
+
+# set up controls for fitting resamples ----
+
+
+# write out split, train, test and folds ----
