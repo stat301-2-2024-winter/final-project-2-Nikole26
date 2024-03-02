7c096235220949b1822ea6cf0cda74eb5b944fdf
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-03-02 13:56:28 -0600

    fix renames

--- r_scripts/2_recipes.R
+++ r_scripts/2_recipes.R
@@ -20,7 +20,7 @@ sba_recipe_1 <-
   step_dummy(all_nominal_predictors(), one_hot = TRUE) |>
   step_zv(all_predictors())
 
-sba_recipe_2 <- 
+sba_recipe_baseline <- 
   recipe(mis_status ~ ., data = sba_train) |>
   step_rm(loan_nr_chk_dgt, name, city, state, bank, bank_state, approval_date, chg_off_date, disbursement_date) |>
   step_unknown(all_nominal_predictors()) |>
@@ -36,4 +36,4 @@ sba_recipe_1 |>
 
 # write out recipe(s) ----
 save(sba_recipe_1, file = here("recipes/sba_recipe_1.rda"))
-save(sba_recipe_2, file = here("recipes/sba_recipe_2.rda"))
+save(sba_recipe_baseline, file = here("recipes/sba_recipe_baseline.rda"))
