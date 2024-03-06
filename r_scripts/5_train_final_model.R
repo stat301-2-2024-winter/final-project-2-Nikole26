6a1ade93aad592727e730beab5579060d3b8ac57
Nikole26 <nikolemonterocervantes2026@u.northwestern.edu>
2024-03-05 18:11:57 -0600

    Final model evalution updated

--- r_scripts/5_train_final_model.R
+++ r_scripts/5_train_final_model.R
@@ -13,14 +13,24 @@ load(here("data_splits/sba_train.rda"))
 # Best Model --------
 select_best(rf_tuned_1, metric = "accuracy")
 
-# finalize workflow -----
+# finalize workflow for accuracy-----
 final_wflow <- rf_tuned_1 |>
   extract_workflow(rf_tuned_1) |>
   finalize_workflow(select_best(rf_tuned_1, metric = "accuracy"))
+
+# finalize workflow for roc-----
+final_wflow_roc <- rf_tuned_1 |>
+  extract_workflow(rf_tuned_1) |>
+  finalize_workflow(select_best(rf_tuned_1, metric = "roc_auc"))
 
 # train final model-----
 #set seed
 set.seed(301)
 final_fit <- fit(final_wflow, sba_train)
 
+set.seed(103)
+final_fit_roc <-  fit(final_wflow_roc, sba_train)
+
+# saving results-------
 save(final_fit, file = here("results/final_fit.rda"))
+save(final_fit_roc, file = here("results/final_fit_roc.rda"))
--- r_scripts/6_assess_final_model.R
+++ r_scripts/6_assess_final_model.R
@@ -11,18 +11,19 @@ library(janitor)
 load(here("results/rf_tuned_1.rda"))
 load(here("data_splits/sba_test.rda"))
 load(here("results/final_fit.rda"))
+load(here("results/final_fit_roc.rda"))
 
 # Hadling missingness in sba test data
-#sba_test_tidy <- sba_test |>
-#  mutate(
-# approval_fy = ifelse(is.na(approval_fy), mean(approval_fy, na.rm = TRUE), approval_fy),
-#    new_exist = ifelse(is.na(new_exist), mean(new_exist, na.rm = TRUE), new_exist)
-#  )
-sba_test <- sba_test |>
-  mutate(across(where(is.numeric), ~ifelse(is.na(.), mean(., na.rm = TRUE), .))) |>
-  mutate(across(where(is.factor), ~ifelse(is.na(.), table(.)[which.max(table(.))], .)))
+sba_test_tidy <- sba_test |>
+  mutate(
+ approval_fy = ifelse(is.na(approval_fy), mean(approval_fy, na.rm = TRUE), approval_fy),
+    new_exist = ifelse(is.na(new_exist), mean(new_exist, na.rm = TRUE), new_exist)
+  )
+#sba_test <- sba_test |>
+#  mutate(across(where(is.numeric), ~ifelse(is.na(.), mean(., na.rm = TRUE), .))) |>
+#  mutate(across(where(is.factor), ~ifelse(is.na(.), table(.)[which.max(table(.))], .)))
 
-# Predictions -----------
+# Predictions using accuracy-----------
 pred_class_rf <- predict(final_fit, sba_test_tidy, type = "class")
 pred_prob_rf <- predict(final_fit, sba_test_tidy, type = "prob")
 sba_results_rf <- sba_test_tidy |>
@@ -39,7 +40,17 @@ acc_rf <- accuracy(sba_results_rf, mis_status, .pred_class) |>
 
 save(acc_rf, file = here("results/acc_rf.rda"))
 
-#Plot-----
+# Confusion Matrix-----
 conf_mat <- conf_mat(sba_results_rf, mis_status, .pred_class)
 
 save(conf_mat, file = here("results/conf_mat.rda"))
+
+# Predictions using roc_auc-----------
+mis_status_pred <- predict(final_fit_roc, sba_test_tidy) |>
+  bind_cols(sba_test |>
+              select(mis_status))
+
+roc_data <- roc_curve(mis_status_pred, mis_status, truth = c("CHGOFF", "PIF"))
+roc_curve(mis_status_pred, mis_status, truth = c(CHGOFF, .pred_class$PIF))
+
+#autoplot(roc_curve())
